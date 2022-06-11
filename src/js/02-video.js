964097911300b115f46c35857641bc8c4eb0512f
Viktoriia Hrytsenko <dev.viktoriia.h@gmail.com>
2022-06-11 19:03:50 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,7 +5,7 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then();
 
 player.on('timeupdate', throttle((data) => {
     localStorage.setItem("videoplayer-current-time", data.seconds);
